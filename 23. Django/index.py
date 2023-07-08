dad0f46ebcdf6c8b693a356c6d63f728e97d8eec
unknown <maseeh.niazaai@gmail.com>
2023-07-08 12:35:24 +0430

    Django added

---
+++ 23. Django/index.py
@@ -0,0 +1,30 @@
+
+
+# -----------------------------------------------------------------
+# To create virtual Environment we use the this command 
+# -----------------------------------------------------------------
+# python -m venv ll_env
+
+# -----------------------------------------------------------------
+# To activate virtual env we use this command  
+# -----------------------------------------------------------------
+# open up bash : source ll_env\Scripts\activate 
+
+
+# -----------------------------------------------------------------
+# To deactivate virtual env we use this command  
+# -----------------------------------------------------------------
+# open up bash : deactivate
+
+
+# -----------------------------------------------------------------
+# To install the Django in the virtual env : open up bash 
+# -----------------------------------------------------------------
+#  ~ pip install --upgrade pip
+#  ~ pip install django
+#  ~ django-admin startproject ll_project .
+#  ~ python manage.py migrate 
+#  ~ python manage.py runserver
+
+
+
