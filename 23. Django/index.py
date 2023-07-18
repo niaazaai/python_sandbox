a5f1ff7f5bddaed2e03866090cd165a09e944c96
unknown <maseeh.niazaai@gmail.com>
2023-07-18 16:08:03 +0430

    new urls and views added

--- 23. Django/index.py
+++ 23. Django/index.py
@@ -8,7 +8,7 @@
 # -----------------------------------------------------------------
 # To activate virtual env we use this command  
 # -----------------------------------------------------------------
-# open up bash : source ll_env\Scripts\activate 
+# open up bash : source ll_env/Scripts/activate
 
 
 # -----------------------------------------------------------------
@@ -25,6 +25,17 @@
 #  ~ django-admin startproject ll_project .
 #  ~ python manage.py migrate 
 #  ~ python manage.py runserver
+#  ~ python manage.py startapp learning_logs <- will setup new files for start working on the project like models.py , views.py and etc.. 
+#  ~ winpty python manage.py createsuperuser <--- to create a super user for admin site
+#  ~ python manage.py shell <---- to start interactive shell 
 
 
+
+# -----------------------------------------------------------------
+# Database Credentials 
+# -----------------------------------------------------------------
+#   Username : root
+#   Password : kingjoker
+#   Email : maseeh.niazaai@gmail.com 
+ 
 
